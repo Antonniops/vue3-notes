d8e18ead4c9f305c9d954a85bf703a49b3623209
Antonio <antonio.pena@externos.tinsa.com>
2025-03-11 14:45:13 +0100

    Make floating buttons

--- src/App.vue
+++ src/App.vue
@@ -43,29 +43,29 @@ const filteredNotes = computed(() => {
       <InputIcon class="pi pi-search" />
       <InputText type="text" v-model="search" placeholder="Buscar nota" />
     </IconField>
-
-    <div>
-      <i
-        class="pi pi-plus text-amber-400 mr-2 hover:cursor-pointer hover:ring-amber-200"
-        style="font-size: 1.5rem"
-        @click="addNote()"
-      />
-      <i
-        class="pi pi-sun text-amber-300 hover:text-amber-200 cursor-pointer hover:ring-amber-200"
-        style="font-size: 1.5rem"
-        @click="toggleDarkMode()"
-        v-if="!isDarkMode"
-      />
-      <i
-        class="pi pi-moon text-gray-300 hover:text-gray-200 cursor-pointer"
-        style="font-size: 1.5rem"
-        @click="toggleDarkMode()"
-        v-if="isDarkMode"
-      />
-    </div>
   </nav>
 
-  <ul class="flex flex-wrap gap-4 mt-5">
+  <div>
+    <i
+      class="pi pi-sun text-amber-300 hover:text-amber-200 cursor-pointer hover:ring-amber-200 fixed top-7 right-7"
+      style="font-size: 1.5rem"
+      @click="toggleDarkMode()"
+      v-if="!isDarkMode"
+    />
+    <i
+      class="pi pi-moon text-gray-300 hover:text-gray-200 cursor-pointer fixed top-7 right-7"
+      style="font-size: 1.5rem"
+      @click="toggleDarkMode()"
+      v-if="isDarkMode"
+    />
+  </div>
+  <i
+    class="pi pi-plus text-green-400 p-2 hover:text-green-300 hover:cursor-pointer hover:ring hover:ring-green-200 rounded-full fixed bottom-7 right-7"
+    style="font-size: 1.5rem"
+    @click="addNote()"
+  />
+
+  <ul class="flex flex-wrap justify-around gap-4 mt-5">
     <li class="list-none" v-for="note in filteredNotes" :key="note.id">
       <vNote :note="note" @delete="deleteNote" />
     </li>
